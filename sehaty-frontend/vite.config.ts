856b6495c2ca911edb223c07a51953d7655a1d45
kabolghar <159119465+kabolghar@users.noreply.github.com>
2025-04-29 00:17:30 +0300

    reconfiguring for vercel

--- sehaty-frontend/vite.config.ts
+++ sehaty-frontend/vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+        target: process.env.VITE_API_URL || '/api',
         changeOrigin: true
       }
     }
